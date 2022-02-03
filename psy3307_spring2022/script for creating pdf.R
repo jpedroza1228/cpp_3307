d0ccdc46d5ef468e332feda2c0ee6adb237cd069
JP <jpedroza@uoregon.edu>
2022-02-02 17:56:42 -0800

    lectures and problem sets for Spring 2022

---
+++ psy3307_spring2022/script for creating pdf.R
@@ -0,0 +1,9 @@
+
+# for pretty docs or just regular rmarkdown files
+# webshot::rmdshot('psy3307_spring2022/syllabus/psy3307_01_syllabus.Rmd', 'psy3307_01_syllabus.pdf')
+
+
+# for slides
+# pagedown::chrome_print('file:///C:/Users/cpppe/Desktop/github_projects/cpp_3307/psy3307_spring2022/lectures/week1_introduction.html#1')
+# pagedown::chrome_print('file:///C:/Users/cpppe/Desktop/github_projects/cpp_3307/psy3307_spring2022/lectures/research_methods.html#1')
+# pagedown::chrome_print('file:///C:/Users/cpppe/Desktop/github_projects/cpp_3307/psy3307_spring2022/lectures/frequencies_central_tendency.html#1')
